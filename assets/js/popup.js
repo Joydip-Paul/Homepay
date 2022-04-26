e5dfadb1b4ce4023309e201eb194338b70091b1c
Joydip Paul <joydippaul72@gmail.com>
2022-04-27 00:37:28 +0600

    sesh

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -3,6 +3,9 @@ let play = document.querySelector('.pulse-button');
 let video = document.querySelector('.video-frame');
 let close = document.querySelector('.close');
 let videoClip = document.querySelector('.video');
+let vid = document.querySelector('.video');
+let body = document.querySelector('.blaze-services');
+
 play.onclick = function () {
     play.classList.add('play-active');
     video.classList.add('video-active');
@@ -11,6 +14,12 @@ play.onclick = function () {
 }
 
 close.onclick = function () {
+    play.classList.remove('play-active');
+    video.classList.remove('video-active');
+    videoClip.pause();
+}
+
+body.onclick = function () {
     play.classList.remove('play-active');
     video.classList.remove('video-active');
     videoClip.pause();
