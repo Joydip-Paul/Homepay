136f7162018a38e316c20ac3de631fa011b8faab
Joydip Paul <joydippaul72@gmail.com>
2022-04-28 00:34:10 +0600

    fine

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 // sticky navbar
-document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            document.getElementById('navbar_top').classList.add('fixed-top');
-        } else {
-            document.getElementById('navbar_top').classList.remove('fixed-top');
-        }
-    });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     window.addEventListener('scroll', function () {
+//         if (window.scrollY > 50) {
+//             document.getElementById('navbar_top').classList.add('fixed-top');
+//         } else {
+//             document.getElementById('navbar_top').classList.remove('fixed-top');
+//         }
+//     });
+// });
 
 // Active nav
 let controls = document.querySelectorAll('.nav-item');
@@ -24,22 +24,6 @@ let controls = document.querySelectorAll('.nav-item');
 const date = new Date();
 const year = date.getFullYear();
 document.getElementById("year").innerHTML = year;
-
-// Scroll Button Start
-var myButton = document.getElementById("myBtn");
-window.onscroll = function () { Jishu() };
-function Jishu() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        myButton.style.display = "block";
-    } else {
-        myButton.style.display = "none";
-    }
-}
-function Joydip() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-// Scroll Button End
 
 
 
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -5,22 +5,34 @@ let close = document.querySelector('.close');
 let videoClip = document.querySelector('.video');
 let vid = document.querySelector('.video');
 let body = document.querySelector('.blaze-services');
+let hero = document.querySelector('.blaze-hero h1');
+let hero2 = document.querySelector('.blaze-hero p');
+let pulse = document.querySelector('.pulse-area');
 
 play.onclick = function () {
     play.classList.add('play-active');
     video.classList.add('video-active');
     videoClip.play();
     videoClip.currentTime = 0;
+    hero.style.opacity = "0";
+    hero2.style.opacity = "0";
+    pulse.style.opacity = "0";
 }
 
 close.onclick = function () {
     play.classList.remove('play-active');
     video.classList.remove('video-active');
     videoClip.pause();
+    hero.style.opacity = "1";
+    hero2.style.opacity = "1";
+    pulse.style.opacity = "1";
 }
 
 body.onclick = function () {
     play.classList.remove('play-active');
     video.classList.remove('video-active');
     videoClip.pause();
+    hero.style.opacity = "1";
+    hero2.style.opacity = "1";
+    pulse.style.opacity = "1";
 }
